5e455e8e36a2ae63e63cc2cfaf46d54f87490d2f
Julie Anderson <juliemanderson@gmail.com>
2016-11-05 10:27:29 -0500

    Stub in notes controller functions

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,3 +1,27 @@
 /**
  * Created by julieanderson on 11/5/16.
  */
+
+ exports.findById = function(req, res) {
+ var id = req.params.id;
+ console.log("Finding Note " + id);
+ }
+
+ exports.findAll = function(req, res) {
+  console.log("Finding All Notes");
+  }
+
+ exports.add = function(req, res) {
+   console.log("Adding Note");
+   }
+
+ exports.update = function(req, res) {
+   var id = req.params.id;
+    console.log("Updating Note " + id);
+   }
+
+  exports.deleteById = function(req, res) {
+    var id = req.params.id;
+     console.log("Deleting Note " + id);
+    }
+
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* API */
+router.get('/api/notes', ctrlNotes.findAll);
+router.get('/api/notes/:id', ctrlNotes.findById);
+router.put('/api/notes/', ctrlNotes.add);
+router.post('/api/notes/:id', ctrlNotes.update);
+router.delete('/api/notes/:id',ctrlNotes.deleteById);
+
 module.exports = router;
