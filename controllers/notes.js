2787572c4afb70514737991dc4508524ab0c640a
Julie Anderson <juliemanderson@gmail.com>
2016-11-05 11:06:04 -0500

    Add index findAll function in notes controller

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,6 +2,24 @@
  * Created by julieanderson on 11/5/16.
  */
 
+ var Db = require('mongodb').Db,
+    Server = require('mongodb').Server,
+    ObjectId = require('mongodb').ObjectId;
+
+ var server = new Server('localhost', 27017, {auto_reconnect: true});
+ db = new Db('test', server);
+
+ db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'test' database");
+        db.collection('notes', {strict:true}, function(err,collection){
+            if(err){
+                console.log("The 'notes' collection doesn't exist.");
+            }
+        });
+     }
+ });
+
  exports.findById = function(req, res) {
  var id = req.params.id;
  console.log("Finding Note " + id);
@@ -9,7 +27,13 @@
 
  exports.findAll = function(req, res) {
   console.log("Finding All Notes");
-  }
+  db.collection('notes', function(err,collection) {
+    collection.find().toArray(function(err, items) {
+        res.status(200);
+        res.send(items);
+    });
+  });
+  };
 
  exports.add = function(req, res) {
    console.log("Adding Note");
