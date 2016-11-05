7a6f3461e74ad7d1fabf3d342565fada741b08fc
Julie Anderson <juliemanderson@gmail.com>
2016-11-05 10:14:29 -0500

    Add controllers - index.js & notes.js

---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by julieanderson on 11/5/16.
+ */
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var ctrlNotes = require('../controllers/notes');
+var ctrlIndex = require('../controllers/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
